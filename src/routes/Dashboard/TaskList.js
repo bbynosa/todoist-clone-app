e15aed41cfe2e3a4486701a2576aa44040900b15
Bruce Baynosa <bruce.baynosa15@gmail.com>
2023-02-17 17:40:35 +0800

    Added initial Dashboard layout and view tasks panel

--- src/routes/Dashboard/TaskList.js
+++ src/routes/Dashboard/TaskList.js
@@ -6,17 +6,35 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Link, Button } from "@mui/material";
-
 import {
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel
+  Link,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  ListItemIcon,
+  ListItemButton,
+  Radio,
+  ButtonGroup,
+  Typography,
+  Grid,
 } from "@mui/material";
+
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material";
 import InlineAdd from "./InlineAdd";
-import { useState } from "react"
+import { useState } from "react";
 import sortByProperty from "../../utils";
+
+// TODO: Can we please store these SVGs outside src/ dir, probably on public/ dir
+import { ReactComponent as EditIcon } from "../../icons/edit.svg";
+import { ReactComponent as DateIcon } from "../../icons/date.svg";
+import { ReactComponent as CommentIcon } from "../../icons/comment.svg";
+import { ReactComponent as ViewIcon } from "../../icons/view.svg";
+import { ReactComponent as OpenMoreIcon } from "../../icons/open-more.svg";
+
+import Spinner from "../../components/Spinner";
+import TaskListItem from "./TaskItem";
 
 const status = ["Not started", "In progress", "Completed"];
 const priorities = ["Urgent", "Important", "Medium", "Low"];
@@ -24,24 +42,36 @@ const priorities = ["Urgent", "Important", "Medium", "Low"];
 // Dictionary of fields that can be used to sort the tasks
 // Format is Field Name => Property Name
 const sortFields = {
-  "Default": "id",
-  "Name": "name",
-  "Status": "status",
-  "Priority": "priority",
-  "Assigned To": "assigned_to"
+  Default: "id",
+  Name: "name",
+  Status: "status",
+  Priority: "priority",
+  "Assigned To": "assigned_to",
 };
 
 // Define a transform function for each sort field
 // If nothing is defined, use the default transform
 const sortFieldTransform = {
-  "Priority": (priority) => priorities.indexOf(priority),
-  "Status": (stat) => status.indexOf(stat)
+  Priority: (priority) => priorities.indexOf(priority),
+  Status: (stat) => status.indexOf(stat),
 };
 
-export default function TaskList({ rows, selectTask, deleteTask, saveTodo, formMode, setRows }) {
+const style = {
+  width: "100%",
+  bgcolor: "background.paper",
+};
 
-  // Define state variables 
+export default function TaskList({
+  rows,
+  selectTask,
+  deleteTask,
+  saveTodo,
+  formMode,
+  setRows,
+}) {
+  // Define state variables
   const [sortField, setSortField] = useState("Default");
+  const [showTaskButtons, setShowTaskButtons] = useState(false);
 
   // Define handlers
   const handleSortChange = (event) => {
@@ -50,93 +80,50 @@ export default function TaskList({ rows, selectTask, deleteTask, saveTodo, formM
     // Set the current sort field
     setSortField(value);
 
-    // Sort the rows 
+    // Sort the rows
     const propertyName = sortFields[value];
-    setRows(value in sortFieldTransform
-      ? sortByProperty(rows, propertyName, sortFieldTransform[value])
-      : sortByProperty(rows, propertyName));
-  }
+    setRows(
+      value in sortFieldTransform
+        ? sortByProperty(rows, propertyName, sortFieldTransform[value])
+        : sortByProperty(rows, propertyName)
+    );
+  };
 
-  // Render UI 
+  // Render UI
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell variant="head">Name</TableCell>
-            <TableCell variant="head" align="left">Status</TableCell>
-            <TableCell variant="head" align="left">Priority</TableCell>
-            <TableCell variant="head" align="left">Assigned to</TableCell>
-            <TableCell>
-              <FormControl fullWidth>
-                <InputLabel id="sort-by-label">Sort By</InputLabel>
-                <Select
-                  labelId="sort-by-label"
-                  label="Sort By"
-                  value={sortField}
-                  onChange={handleSortChange}
-                >
-                  {Object.keys(sortFields).map((sortField) => (
-                    <MenuItem key={sortField} value={sortField}>{sortField}</MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            {/* TODO: Avoid prop drilling. Maybe time to use redux or context */}
-            <InlineAdd saveTodo={saveTodo} formMode={formMode} />
-          </TableRow>
-          {/* TODO: New todo should be first on the list. We can do this by sorting by created_date */}
+    <>
+      <Grid container paddingTop="50px">
+        <Grid item md={4} paddingLeft="90px">
+          <Typography variant="h5" component="h1" sx={{ fontWeight: "bold" }}>
+            Inbox
+          </Typography>
+        </Grid>
+        <Grid item md={8}>
+          <ButtonGroup>
+            <Button>
+              <CommentIcon />
+            </Button>
+            <Button>
+              <ViewIcon />
+            </Button>
+            <Button>
+              <OpenMoreIcon />
+            </Button>
+          </ButtonGroup>
+        </Grid>
+        <List sx={style}>
           {rows.length ? (
             rows.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{
-                  "&:last-child td, &:last-child th": { border: 0 },
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  <Link
-                    href="#"
-                    underline="hover"
-                    onClick={() => {
-                      selectTask(row.id);
-                    }}
-                  >
-                    {row.name}
-                  </Link>
-                </TableCell>
-                <TableCell align="left">{row.status}</TableCell>
-                <TableCell align="left">{row.priority}</TableCell>
-                <TableCell align="left">{row.assigned_to}</TableCell>
-                <TableCell align="left">
-                  {/* TODO: replace to loadingButton MUI component */}
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    onClick={() => deleteTask(row.id)}
-                    color="error"
-                  >
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
+              <>
+                <TaskListItem row={row} />
+                <Divider />
+              </>
             ))
           ) : (
-            <TableRow
-              key="no-data"
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                Loading data
-              </TableCell>
-            </TableRow>
+            <Spinner />
           )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        </List>
+      </Grid>
+    </>
   );
 }
